9d5a00d62fee4e163b06aaf45ffa8c3e8013a8e4
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2025-08-13 16:34:19 +0800

    Add new logo and exit message

---
+++ src/main/java/Meo.java
@@ -0,0 +1,18 @@
+public class Meo {
+    public static void main(String[] args) {
+        String cat = 
+                "      ⢀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⢀⣾⣿⡇⠀⠀⠀⠀⠀⢀⣼⡇\n" +
+                "⠀⠀⠀⠀⠀⣸⣿⣿⡇⠀⠀⠀⠀⣴⣿⣿⠃\n" +
+                "⠀⠀⠀⠀⢠⣿⣿⣿⣇⠀⠀⢀⣾⣿⣿⣿⠀\n" +
+                "⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡟⠀\n" +
+                "⠀⠀⢰⡿⠉⠀⡜⣿⣿⣿⡿⠿⢿⣿⣿⠃⠀\n" +
+                "⠒⠒⠸⣿⣄⡘⣃⣿⣿⡟⢰⠃⠀⢹⣿⡇⠀\n" +
+                "⠚⠉⠀⠈⠻⣿⣿⣿⣿⣿⣮⣤⣤⣿⡟⠁⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠛⠛⠛⠁⠀⠒⠤\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠀⠀";
+        System.out.println("Hello from Meo\n" + cat);
+        System.out.println("---------------------------------");
+        System.out.println("Bye, it's my nap time.");
+    }
+}
